04b03da9d36718b5fd8ea843c90d306e8ae45646
Juanan Fernandez <juanan.fernandez@gmail.com>
2022-03-29 18:38:20 +0200

    Card wrapper component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import Card from '../Card/Card';
 import Calendar from '../Calender/Calender';
 import './ExpenseItem.css';
 
 export default function ExpenseItem({ expense }) {
 	return (
-		<div className='expense-item'>
+		<Card className='expense-item'>
 			<Calendar date={expense.date} />
 			<div className='expense-item__description'>
 				<h2>{expense.title}</h2>
 				<div className='expense-item__price'>{Number(expense.amount).toFixed(2)} EUR</div>
 			</div>
-		</div>
+		</Card>
 	);
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
 import './Expenses.css';
 import data from '../../data/expensesData';
+import Card from '../Card/Card';
 
 export default function Expenses() {
 	return (
-		<div className='expenses-list'>
+		<Card className='expenses-list'>
 			{data.map(expense => (
 				<ExpenseItem key={expense.id} expense={{ ...expense }} />
 			))}
-		</div>
+		</Card>
 	);
 }
